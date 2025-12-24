91d2c2aad6d8ce894af6aee386439ccefca2aa43
lesha2r <leshatour@gmail.com>
2025-12-24 19:22:59 +0300

    test: actualized

--- src/__tests__/Kodzero.test.ts
+++ src/__tests__/Kodzero.test.ts
@@ -2,7 +2,8 @@ import Kodzero from '../Kodzero.js';
 
 it('should create an instance if host provided', async () => {
     const kodzero = new Kodzero({
-        host: 'http://localhost:6969'
+        host: 'http://localhost:6969',
+        collection: "_auth_"
     });
 
     expect(kodzero).toBeInstanceOf(Kodzero);
