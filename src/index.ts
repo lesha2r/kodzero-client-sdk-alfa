530bc8d52294634edc81708c0fde1b7ab471f74e
lesha2r <leshatour@gmail.com>
2026-08-28 16:47:18 +0300

    feat: add types export for ModelClass, ModelInstance

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Kodzero from "./Kodzero.js";
 export { JsonField } from "./utils/parseDataBySchema.js";
+export type { ModelClass, ModelInstance } from "./model/createModel.js";
 
 export default Kodzero
